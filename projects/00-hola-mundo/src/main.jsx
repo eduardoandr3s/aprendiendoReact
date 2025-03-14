ce59816b60e1c3475ecceb2406487ebc3acb6f92
EduAndr3s <eduardoandres.segoviaroman@cieep.com>
2025-03-14 20:07:14 +0100

    primeros pasos con react

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode, React } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+  
+  <React.Fragment>
+     <button>Hola button</button>
+      <button>Hola button</button>
+    </React.Fragment>
+   
+
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+)
